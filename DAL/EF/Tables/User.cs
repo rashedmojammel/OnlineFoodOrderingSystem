af85d3de8fc1b854e59394c8afc6afa8b3553938
rashedmojammel <rashedmojamme56@gmail.com>
2026-05-09 01:18:29 +0600

    feat : setting up 3 tier architecture

---
+++ DAL/EF/Tables/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.EF.Tables;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+}
