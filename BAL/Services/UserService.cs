b32975f7116c61aeddc8d0bb88660d2974a0ab84
rashedmojammel <rashedmojamme56@gmail.com>
2026-05-09 12:38:50 +0600

    feat : added user servies

--- BAL/Services/UserService.cs
+++ BAL/Services/UserService.cs
@@ -1,10 +1,54 @@
-﻿using System;
+﻿using AutoMapper;
+using BAL.DTOs;
+using BLL;
+using DAL.EF.Tables;
+using DAL.Repos;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BAL.Services
 {
-    internal class UserService
+    public class UserService
     {
+        UserRepo repo;
+        Mapper mapper;
+
+        public UserService(UserRepo repo)
+        {
+            this.repo = repo;
+            mapper = MapperConfig.GetMapper();
+        }
+
+        public List<UserDTO> Get()
+        {
+            var data = repo.Get();
+            var res = mapper.Map<List<UserDTO>>(data);
+            return res;
+        }
+
+        public UserDTO Get(int id)
+        {
+            var data = repo.Get(id);
+            var res = mapper.Map<UserDTO>(data);
+            return res;
+        }
+
+        public bool Create(UserDTO u)
+        {
+            var converted = mapper.Map<User>(u);
+            return repo.Create(converted);
+        }
+
+        public bool Update(UserDTO u)
+        {
+            var converted = mapper.Map<User>(u);
+            return repo.Update(converted);
+        }
+
+        public bool Delete(int id)
+        {
+            return repo.Delete(id);
+        }
     }
 }
--- DAL/Repos/UserRepo.cs
+++ DAL/Repos/UserRepo.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using DAL.EF;
+using DAL.EF.Tables;
 using System.Collections.Generic;
-using System.Text;
 
 namespace DAL.Repos
 {
-    internal class UserRepo
+    public class UserRepo
     {
+        OnlineFoodOrderingSystemDbContext db;
+
+        public UserRepo(OnlineFoodOrderingSystemDbContext db)
+        {
+            this.db = db;
+        }
+
+        public bool Create(User u)
+        {
+            db.Users.Add(u);
+            return db.SaveChanges() > 0;
+        }
+
+        public User Get(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public List<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+        public bool Update(User u)
+        {
+            var exobj = Get(u.Id);
+            db.Entry(exobj).CurrentValues.SetValues(u);
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            var exobj = Get(id);
+            db.Users.Remove(exobj);
+            return db.SaveChanges() > 0;
+        }
     }
 }
