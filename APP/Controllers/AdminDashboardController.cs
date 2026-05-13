32b1ed09e050a7ef9b96894ccd7b6ab2dc6e6d03
rashedmojammel <rashedmojamme56@gmail.com>
2026-05-14 01:21:12 +0600

    feat : added crud operation for user in admin controller

--- APP/Controllers/AdminDashboardController.cs
+++ APP/Controllers/AdminDashboardController.cs
@@ -1,10 +1,21 @@
 ﻿
+using BAL.DTOs;
+using BAL.Services;
+using Humanizer.DateTimeHumanizeStrategy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace APP.Controllers
 {
     public class AdminDashboardController : Controller
     {
+
+        UserService userService;
+        public AdminDashboardController(UserService userService) {
+            this.userService = userService;
+
+        }
+
         public IActionResult Index()
         {
             if (HttpContext.Session.GetString("UserName") != null)
@@ -17,5 +28,86 @@ namespace APP.Controllers
             }
             return View();
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Users = userService.Get();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(UserDTO u)
+        {
+            // Logic to create a new user (admin or customer) using the UserService
+            // This is just a placeholder and should be implemented according to your application's requirements
+            // For example, you might want to check the role and call different methods in the UserService
+
+            if (ModelState.IsValid)
+            {
+                var res = userService.Create(u);
+                if (res == true)
+                {
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // Handle creation failure (e.g., show an error message)
+                    ModelState.AddModelError("", "Failed to create user.");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var user = userService.Get(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+        [HttpPost]
+        public IActionResult Edit(UserDTO u)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = userService.Update(u);
+                if (res == true)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // Handle update failure (e.g., show an error message)
+                    ModelState.AddModelError("", "Failed to update user.");
+                }
+            }
+            return View(u);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var user = userService.Get(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id, string Decision)
+        {
+            if (Decision.Equals("Yes"))
+            {
+                var user = userService.Delete(id);
+                if (user == true)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+
+                    ModelState.AddModelError("", "Failed to delete user.");
+            }
+            return RedirectToAction("Index");
+        }
+         
     }
 }
