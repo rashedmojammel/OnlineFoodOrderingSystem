ab422c5c0535aa39ddadbe8e78ca385edfb9399e
rashedmojammel <rashedmojamme56@gmail.com>
2026-05-16 10:49:32 +0600

    feat : added dtos for order,orderitem and cart

---
+++ BAL/DTOs/OrderDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAL.DTOs
+{
+    public class OrderDTO
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string? Status { get; set; }
+        public decimal? Total { get; set; }
+        public List<OrderItemDTO> OrderItems { get; set; } = new();
+    }
+}
